32cfc937e6d93a1dee1a8b71cc1eb8ef7a768185
naz-coder <favouritenaz@yahoo.com>
2023-03-19 14:23:17 +0100

    Gallery with Slides and Footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,21 @@
 // Mobile menu slide-in DOM manipulation
 document.querySelector('.menu-btn').addEventListener('click', ()=> document.querySelector('.main-menu').classList.toggle('show'));
 
-// Picture carousel function
-var slidesIndex = 0;
-// slideShow();
-// const styling = ;
-const slideShow = function () {
-    var s = 0;
-    var numberOfImages = document.getElementsByClassName('.slide-item');
-    for (s = 0; s < numberOfImages.length; s++){
-        numberOfImages[s].styling.display = "none";
-    }
-    slidesIndex++;
-    if (slidesIndex > numberOfImages.length){
-        slidesIndex = 1
-    }
-    numberOfImages[slidesIndex -1].styling.display = "block";
-    setTimeout (slideShow, 2000)
-}
-console.log(slideShow('Coming soon'))
 
-   
+// Picture carousel function
+const imageArray = [
+    'assets/img/biography.jpg',
+    'assets/img/republic1.jpg',
+    'assets/img/songhero.jpeg',
+];
+
+let slideIndex = 0;
+
+const slideShow = function () {
+    document.getElementById('slide1').src = imageArray[slideIndex];
+    slideIndex++;
+    if (slideIndex == imageArray.length) { slideIndex = 0; }
+    setTimeout("slideShow()", 3000);
+}
+slideShow();
+
